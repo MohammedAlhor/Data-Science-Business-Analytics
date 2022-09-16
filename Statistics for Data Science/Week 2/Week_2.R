1a37234dd6927df515d145894b02ed68081ac360
MohammedAlhor <m_alhor@hotmail.com>
2022-09-16 12:28:22 +0200

    Week 3 statistics

---
+++ Statistics for Data Science/Week 2/Week_2.R
@@ -0,0 +1,6 @@
+# R Werkbestand college 16-09
+
+interval <- mean =(x) + qt(c(alpha))
+
+# Assigment 3.1
+
---
+++ Statistics for Data Science/Week_3/Week_3.R
@@ -0,0 +1,9 @@
+# Assigment 3.1
+
+data <- rnorm(100, mean = 0.05, sd=1)
+
+mean(data)
+
+hist(data)
+
+p1 <- t.test(data,mu=0)
