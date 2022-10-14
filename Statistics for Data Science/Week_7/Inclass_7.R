70093729e87bdc3dbabde8cfcfa1fd8363d0e9c1
MohammedAlhor <m_alhor@hotmail.com>
2022-10-14 11:21:21 +0200

    adf

--- Statistics for Data Science/Week_7/Inclass_7.R
+++ Statistics for Data Science/Week_7/Inclass_7.R
@@ -1,7 +1,18 @@
 load("~/Documents/Data-Science-Business-Analytics/Data/website.RData")
 #
-glm(active~income+age+region, family=binomial(link=probit), data = website) 
-# because 
+
+logit<-glm(active~income+age+region, family=binomial(link=probit), data = website) 
+glm(active~income+age+relevel(region,3), family=binomial(link=probit), data = website) 
+summary(logit)
+# getallen zijn niet zo eenvoudig te interpreteren
+# Tekens kun je wel interpreteren, min of plus.
+# onderling kkun je ze wel vergelijken
+# Regio niet significant maar het verschil tussen de regios is wel significant
+
+# voorspelling
+plot(website$age, predict(logit, type = "response"))
+
+
 glm <- glm(active~log(income)+log(age)+region, family=binomial(link=probit), data = website) 
 #income, age, region2, region3
 
