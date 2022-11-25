fc6ea21a085c7e6120769d848816a8cad5959d02
MohammedAlhor <m_alhor@hotmail.com>
2022-11-25 12:06:44 +0100

    asd

---
+++ Predictive Modeling Forecasting/Eindopdracht/Eindopdracht.R
@@ -0,0 +1,19 @@
+library(dplyr)
+# Inladen data
+# russia_losses_equipment_correction.csv
+# russia_losses_equipment.csv
+# russia_losses_personnel.csv
+
+russia_losses_equipment <- read_csv("Data/russia_losses_equipment.csv")
+
+plot(russia_losses_equipment$aircraft)
+
+
+# first differences, moet eruit gehaald worden
+
+russia_losses_equipment$aircraft_lag <- lag(russia_losses_equipment$aircraft)
+russia_losses_equipment$aircraft_dif <- russia_losses_equipment$aircraft - russia_losses_equipment$aircraft_lag
+plot(russia_losses_equipment$aircraft_dif)
+
+
+plot(russia_losses_personnel$personnel)
---
+++ Predictive Modeling Forecasting/Week_4/Inclass.R
@@ -0,0 +1,7 @@
+
+# Breusch godfrey test on autocorrelation between residuals
+# Constanat variance, homoskedacticity is an assumption
+# How do we deal with non constant variances, take the square of residuals. 
+# For forecasting try to use an interval prediction
+# Point forecasting
+# 
