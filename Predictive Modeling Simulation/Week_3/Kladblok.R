212f6783174dde44101b5320a23dd82f5de4bbf8
MohammedAlhor <m_alhor@hotmail.com>
2023-01-20 16:06:27 +0100

    asd

--- Predictive Modeling Simulation/Week_3/Kladblok.R
+++ Predictive Modeling Simulation/Week_3/Kladblok.R
@@ -2,5 +2,20 @@
 x <- as.integer(runif(numcases, min, max+1))
 sample(6,1)
 
+set.seed(42)
+vliegtuig
+
+x<-rpois(1, 1)
+i <- 0 
+while (i < 5) {
+  x<-rpois(1,1)
+  x[i] 
+}
+
+x<-rpois(1,1)
 
 
+teller bijhouden
+
+globale variabelen
+system state
